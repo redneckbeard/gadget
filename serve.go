4e4ffc6281efaf9988a30d408c0fcbf487cb359e
Jonathan Lukens <jonathan.lukens@americastestkitchen.com>
2013-04-12 19:49:33 -0400

    Push configuration into a Gadget type (one step closer to easy end-to-end tests.

--- serve.go
+++ serve.go
@@ -5,9 +5,24 @@ import (
 	"net/http"
 )
 
+var app Gadget
+
+type Gadget interface {
+	OnStart() error
+}
+
+func SetApp(g Gadget) {
+	app = g
+}
+
 func Go(port string) {
-	err := env.Configure()
-	if err != nil {
+	if err := env.Configure(); err != nil {
+		panic(err)
+	}
+	if app == nil {
+		panic("No call to SetApp found. Ensure that you've imported your app package you are calling SetApp outside of main.")
+	}
+	if err := app.OnStart(); err != nil {
 		panic(err)
 	}
 	env.ServeStatic()
