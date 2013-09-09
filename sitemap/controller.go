e9dbd4add0e604f63959fab6742e7c0bfaf897a9
Jonathan Lukens <jonathan.lukens@americastestkitchen.com>
2013-09-08 23:22:51 -0400

    Add sitemap package.

---
+++ sitemap/controller.go
@@ -0,0 +1,32 @@
+package sitemap
+
+import (
+	"github.com/redneckbeard/gadget"
+)
+
+func init() {
+	gadget.Register(&SitemapController{})
+}
+
+type SitemapController struct {
+	*gadget.DefaultController
+}
+
+func (c *SitemapController) IdPattern() string { return `[\w-]+` }
+
+func (c *SitemapController) Index(r *gadget.Request) (int, interface{}) {
+	index := NewSitemapIndex()
+	response := gadget.NewResponse(index)
+	response.Headers.Set("Content-Type", "text/xml")
+	return 200, response
+}
+
+func (c *SitemapController) Show(r *gadget.Request) (int, interface{}) {
+	if urlset, found := urlSets[r.UrlParams["sitemap_id"]]; !found {
+		return 404, ""
+	} else {
+		response := gadget.NewResponse(urlset)
+		response.Headers.Set("Content-Type", "text/xml")
+		return 200, response
+	}
+}
---
+++ sitemap/sitemap.go
@@ -0,0 +1,77 @@
+package sitemap
+
+import (
+	"encoding/xml"
+	"net/url"
+	"time"
+	"path"
+)
+
+const NAMESPACE = "http://www.sitemaps.org/schemas/sitemap/0.9"
+
+var (
+	Host string
+	urlSets  = make(map[string]*UrlSet)
+)
+
+func Add(name string, urls ...*Url) {
+	urlSet := NewUrlSet(urls...)
+	urlSets[name] = urlSet
+}
+
+type SitemapIndex struct {
+	XMLName   xml.Name   `xml:"sitemapindex"`
+	Namespace string     `xml:"xmlns,attr"`
+	Sitemaps  []*Sitemap `xml:"sitemap"`
+}
+
+func NewSitemapIndex() *SitemapIndex {
+	index := &SitemapIndex{Namespace: NAMESPACE}
+	u, _ := url.Parse(Host)
+	for k, urls := range urlSets {
+		u.Path = path.Join("sitemaps", k)
+		index.Sitemaps = append(index.Sitemaps, &Sitemap{
+			Loc:     u.String(),
+			LastMod: urls.LastMod,
+		})
+	}
+	return index
+}
+
+type Sitemap struct {
+	Loc     string    `xml:"loc"`
+	LastMod time.Time `xml:"lastmod,omitempty"`
+}
+
+type UrlSet struct {
+	XMLName   xml.Name  `xml:"urlset"`
+	Namespace string    `xml:"xmlns,attr"`
+	LastMod   time.Time `xml:"-"`
+	Urls      []*Url    `xml:"url"`
+}
+
+func NewUrlSet(urls ...*Url) *UrlSet {
+	urlSet := &UrlSet{Namespace: NAMESPACE}
+	var latest time.Time
+	for _, u := range urls {
+		loc, _ := url.Parse(u.Loc)
+		if !loc.IsAbs() {
+			loc, _ = url.Parse(Host)
+			loc.Path = u.Loc
+		}
+		u.Loc = loc.String()
+		if u.LastMod.After(latest) {
+			latest = u.LastMod
+		}
+		urlSet.Urls = append(urlSet.Urls, u)
+	}
+	urlSet.LastMod = latest
+	return urlSet
+}
+
+type Url struct {
+	Loc        string    `xml:"loc"`
+	LastMod    time.Time `xml:"lastmod,omitempty"`
+	ChangeFreq string    `xml:"changefreq,omitempty"`
+	Priority   float64   `xml:"priority,omitempty"`
+}
